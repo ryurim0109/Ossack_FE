0050177f55be52ad3e198695638a24799e66cedd
ryurim0109 <dbfla5036@naver.com>
2022-04-25 22:45:53 +0900

    feat. add Header

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Main() {
   return (
     <React.Fragment>
+      <Header/>
         <p>메인~</p>
     </React.Fragment>
   );
