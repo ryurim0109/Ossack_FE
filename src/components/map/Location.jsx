6b0094bfc3a5f17812a9d39264d01d0f6e7c486f
ryurim0109 <dbfla5036@naver.com>
2022-04-27 21:42:34 +0900

    map생성

--- src/components/map/Location.jsx
+++ src/components/map/Location.jsx
@@ -14,6 +14,31 @@ const Location = () => {
 		};
         const map = new kakao.maps.Map(container, options);
 
+        // 지도에 마커와 인포윈도우를 표시하는 함수입니다
+        const displayMarker=(locPosition, message)=> {
+
+            // 마커를 생성합니다
+            let marker = new kakao.maps.Marker({  
+                map: map, 
+                position: locPosition
+            }); 
+            
+            let iwContent = message, // 인포윈도우에 표시할 내용
+                iwRemoveable = true;
+
+            // 인포윈도우를 생성합니다
+            let infowindow = new kakao.maps.InfoWindow({
+                content : iwContent,
+                removable : iwRemoveable
+            });
+            
+            // 인포윈도우를 마커위에 표시합니다 
+            infowindow.open(map, marker);
+            
+            // 지도 중심좌표를 접속위치로 변경합니다
+            map.setCenter(locPosition);      
+        } 
+
         // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
         if (navigator.geolocation) {
             
@@ -39,30 +64,7 @@ const Location = () => {
                 displayMarker(locPosition, message);
         }
 
-        // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-        const displayMarker=(locPosition, message)=> {
-
-            // 마커를 생성합니다
-            let marker = new kakao.maps.Marker({  
-                map: map, 
-                position: locPosition
-            }); 
-            
-            let iwContent = message, // 인포윈도우에 표시할 내용
-                iwRemoveable = true;
-
-            // 인포윈도우를 생성합니다
-            let infowindow = new kakao.maps.InfoWindow({
-                content : iwContent,
-                removable : iwRemoveable
-            });
-            
-            // 인포윈도우를 마커위에 표시합니다 
-            infowindow.open(map, marker);
-            
-            // 지도 중심좌표를 접속위치로 변경합니다
-            map.setCenter(locPosition);      
-        } 
+        
                 
             }, []);
 
