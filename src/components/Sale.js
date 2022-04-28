2376548d2d7759228af2fee9e1779ba5dc4a69c1
ryurim0109 <dbfla5036@naver.com>
2022-04-28 12:12:47 +0900

    [feat] history추가_yurim(220428)

--- src/components/Sale.js
+++ src/components/Sale.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Grid,Text,Input,Button  } from '../elements/index';
+import {history} from '../redux/configStore';
+import SearchImg from '../static/images/searchicon.png';
 
 function Sale() {
   const search =()=>{
@@ -13,14 +15,19 @@ function Sale() {
           <Text size="1.7rem">네이밍</Text>
         </Grid>
         <Grid width="100%" margin="25px 0" bg="#c4c4c4">
-          <Text size="1.85rem" bold>어떤 오피스 공간을 <br/>찾고 계시나요?</Text>
+          <Text size="1.85rem" bold> 닉네임님 어떤 오피스 공간을 <br/>찾고 계시나요?</Text>
         </Grid>
         <Grid width="100%" margin="16px 0" position="relative" >
-          <Input padding="8px" bg="#c4c4c4" placeholder="검색어" _onChange={(e)=>{
-              console.log(e.target.value);
-          }}/>
-          <Grid  width="50px" position="absolute" top="0" right="0">
-            <Button width="50px" height="40px" _onClick={search}>검색</Button>
+         <Grid  width="50px" position="absolute" top="12px"  left="10px">
+            <img src={SearchImg} alt="검색"/>
+            </Grid>
+          <Grid width="100%" height="48px" border="1px solid #111" textIn="40px"  display="flex" cursor="pointer"
+          alignItems="center" color="#999" borderRadius="10px"
+           _onClick={()=>{
+             console.log('zz')
+             history.push('/map')
+          }}>
+            장소, 주소, 건물명 검색
           </Grid>
           
         </Grid>
