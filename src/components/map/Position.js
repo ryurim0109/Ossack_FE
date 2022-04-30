2459d462f2277a26026077626962a46227fa39a0
devkevinsoon <parkts7777@gmail.com>
2022-04-30 15:56:31 +0900

    [feat] API 추가_pts20220430

--- src/components/map/Position.js
+++ src/components/map/Position.js
@@ -1,28 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Axios from "axios";
-import { useDispatch } from 'react-redux';
-import {  actionCreators as  mapActions  } from "../../redux/modules/map";
-
-
+import { useDispatch } from "react-redux";
+import { actionCreators as mapActions } from "../../redux/modules/map";
 
 const Position = (props) => {
-    const { kakao } = window;
-    const dispatch= useDispatch();
-    
-    const {pos,map} =props;
-    //console.log(pos,map)
-    let kakaoMap = map;
-    useEffect(()=>{
-       // console.log(pos)
-        dispatch(mapActions.getOfficeData(pos))
+  const { kakao } = window;
+  const dispatch = useDispatch();
 
-    },[pos])
+  const { pos, map } = props;
+  //console.log(pos,map)
+  let kakaoMap = map;
+  useEffect(() => {
+    // console.log(pos)
+    dispatch(mapActions.getOfficeData(pos));
+  }, [pos]);
 
-    return (
-        <React.Fragment>
-            
-        </React.Fragment>
-    );
+  return <React.Fragment></React.Fragment>;
 };
 
 export default Position;
