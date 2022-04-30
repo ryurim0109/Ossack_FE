601acfd7abfeb9077680a6b21c373d15a30060d9
ryurim0109 <dbfla5036@naver.com>
2022-04-30 22:11:13 +0900

    [feat] 선택시 color 변경_yurim[220430]

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import {Grid,Button,Text,Image} from '../elements/index';
 import {history} from '../redux/configStore';
@@ -6,21 +6,42 @@ import HomeImg from '../static/images/home.png'
 
 
 function Bar() {
+  const [activeNav, setActiveNav] = useState(1);
   return (
     <React.Fragment>
         <Container>
-            <Grid width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <Grid  _onClick={() => {
+              setActiveNav(1)
+              history.push('/main')
+          }}
+            width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                 <Grid width="24px" height="24px"><img src={HomeImg} alt="홈아이콘"/> </Grid>
-                <Text color="#111" size="12px">홈</Text>
+                {activeNav===1 ?  (<Text cursor="pointer" color="red" size="12px">홈</Text>) :
+                 (<Text cursor="pointer" color="#111" size="12px">홈</Text>)
+                }
+               <div/>
             </Grid>
-            <Grid width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <Grid  _onClick={() => {
+              setActiveNav(2)
+              history.push('/main')
+          }}
+            width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                 <Grid width="24px" height="24px" bg="red"></Grid>
-                <Text color="#111" size="12px">찜</Text>
+                {activeNav===2 ?  (<Text cursor="pointer" color="red" size="12px">찜</Text>) :
+                 (<Text cursor="pointer" color="#111" size="12px">찜</Text>)
+                }
             </Grid>
-            <Grid width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <Grid _onClick={() => {
+              setActiveNav(3)
+              history.push('/mypage')
+          }}
+            width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                 <Grid width="24px" height="24px" bg="red"></Grid>
-                <Text color="#111" size="12px">마이페이지</Text>
-            </Grid>{/* 
+                {activeNav===3 ?  (<Text cursor="pointer" color="red" size="12px">마이페이지</Text>) :
+                 (<Text cursor="pointer" color="#111" size="12px">마이페이지</Text>)
+                }
+            </Grid>
+            {/* 
             <Grid width="25%" textAlign="center" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                 <Grid width="24px" height="24px" bg="red"></Grid>
                 <Text color="#111" size="12px">더보기</Text>
@@ -42,5 +63,6 @@ const Container = styled.div`
   z-index:99;
   display:flex;
   justify-content:space-between;
+
 `;
 export default Bar;
