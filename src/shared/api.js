369e60f047f30bc7444d4be675310c7886af4dfb
ryurim0109 <dbfla5036@naver.com>
2022-04-30 15:07:16 +0900

    [feat] api 파일 생성 _yr[220430]

---
+++ src/shared/api.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+//1. axios 인터셉터 생성 
+export const instance = axios.create({
+
+  //15.164.96.141:8080
+  //52.79.228.83:8080
+
+  baseURL: '',
+  headers: {
+    'content-type': 'application/json; charset=UTF-8',
+    accept: 'application/json',
+  },
+  withCredentials: false,
+});
+
+//2. 요청 인터셉터
+instance.interceptors.request.use(
+  //요청직전 호출
+  config => {
+    const Token = localStorage.getItem('token');
+    if (Token === '') {
+      window.alert('로그인을 먼저 해주세요!')
+      return config;
+    }
+    // const tokens = Token.split('=')[1];
+    config.headers = {
+      'content-type': 'application/json;charset=UTF-8',
+      accept: 'application/json',
+      Authorization: `${Token}`,
+    };
+    return config;
+  },
+  //에러 전 호출
+  err => {
+    console.log(err);
+  }
+);
+
+//3. 응답 인터셉터
+instance.interceptors.response.use(
+  success => {
+    const response = success.data;
+
+    if (response.statusCode === 200 && response.responseMessage === '게시글 조회 성공') {
+      return response.posts;
+    }
+
+
+    return success;
+  },
+  error => {
+    console.log(error);
+
+    if (error.response.status === 403 && error.response.responseMessage === '권한이 없습니다.') {
+      window.alert('권한이 없습니다.');
+    }
+
+    if (error.response.status === 404 && error.response.responseMessage === '게시글을 찾을 수 없습니다.') {
+      window.alert('게시글을 찾을 수 없습니다.');
+    }
+
+    if (error.response.data.statusCode === 403 && error.response.data.responseMessage === '권한이 없습니다.') {
+      return window.alert('권한이 없습니다.');
+    }
+
+    return error;
+  }
+);
+
+
+
+export const TokenCheck = localStorage.getItem('token') ? true : false;
