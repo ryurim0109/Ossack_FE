f9eca7a778987833e39e6de6c62388b5c1de584f
ryurim0109 <dbfla5036@naver.com>
2022-04-30 20:18:04 +0900

    [style]검색페이지 완료_yurim[220430]

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Grid,Button,Input} from '../elements/index';
+import {history} from '../redux/configStore';
+
+
+
+const Search = () => {
+    return (
+        <React.Fragment>
+            <Grid  width="100%" height="80px" bg="#fff" padding="0 16px" display="flex" alignItems="center" justifyContent="space-between">
+                <Button is_back _onClick={
+                    ()=>{
+                        history.push("/main");
+                    }
+                }/>
+                    <Input height="48px" width="90%" margin="16px 5%" padding=" 0 5px" inputFocusOutline="none"
+                    placeholder='장소, 근처 역을 입력하세요.' />
+             
+                <Button is_filter/>
+            </Grid>
+        </React.Fragment>
+    );
+};
+
+export default Search;
