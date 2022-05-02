89380db67ce58f3b9cefdc57bbc9e6c49543850c
ryurim0109 <dbfla5036@naver.com>
2022-05-02 16:22:45 +0900

    [style] 마이페이지 중간 뷰 완성_yr[220502]

---
+++ src/components/my/MyContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Button,Grid,Text} from '../../elements/index';
+
+const MyContent = () => {
+    return (
+        <React.Fragment>
+            <Grid width="100%" height="135px" bg="#ccc" padding="0 16px"
+            position="relative" display="flex" flexDirection="column" alignItems="center" justifyContent="center"
+            >
+                <Grid display="flex" alignItems="center" justifyContent="space-between">
+                    <Grid  display="flex" alignItems="center">
+                        <Grid width="24px" margin="0 10px 0 0" height="24px" bg="red"> </Grid>
+                        <Text size="1.250rem" cursor="pointer">오싹 이용 가이드</Text>
+                    </Grid>
+                    <Button is_right/>
+                </Grid>
+                <Grid display="flex" alignItems="center" justifyContent="space-between">
+                    <Grid  display="flex" alignItems="center">
+                        <Grid width="24px" margin="0 10px 0 0" height="24px" bg="red"> </Grid>
+                        <Text size="1.250rem" cursor="pointer">팀원 소개</Text>
+                    </Grid>
+                    <Button is_right/>
+                </Grid>
+                <Grid display="flex" alignItems="center" justifyContent="space-between">
+                    <Grid  display="flex" alignItems="center">
+                        <Grid width="24px" margin="0 10px 0 0" height="24px" bg="red"> </Grid>
+                        <Text size="1.250rem" cursor="pointer">계정 엑세스</Text>
+                    </Grid>
+                    <Button is_right/>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+};
+
+export default MyContent;
