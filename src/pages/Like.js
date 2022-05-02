6df3d694ce2f098480305dcf742af5e1f62358e2
devkevinsoon <parkts7777@gmail.com>
2022-05-03 01:34:36 +0900

    [style]  my list 구조 추가_pts20220503

--- src/pages/Like.js
+++ src/pages/Like.js
@@ -2,12 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Bar } from "../components/home";
 import { MyHeader } from "../components/my/index";
+import {
+  SaleList,
+  HotPlaceList,
+  Office,
+  OfficeLike,
+} from "../components/list/index";
 
-const Like = () => {
+const Like = (props) => {
   return (
     <React.Fragment>
       <Outter>
-        <MyHeader />
+        <MyHeader>My List</MyHeader>
+        <OfficeLike />
         <Bar />
       </Outter>
     </React.Fragment>
