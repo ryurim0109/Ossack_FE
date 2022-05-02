339f11b3789360358b28c8acd45e0cf32124e165
devkevinsoon <parkts7777@gmail.com>
2022-05-03 00:14:23 +0900

    [feat] Like page 추가_pts20220503

--- src/pages/Like.js
+++ src/pages/Like.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Bar} from '../components/home';
+import React from "react";
+import styled from "styled-components";
+import { Bar } from "../components/home";
+import { MyHeader } from "../components/my/index";
 
 const Like = () => {
-    return (
-        <React.Fragment>
-            <Outter>
-                찜
-                <Bar/>
-            </Outter>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Outter>
+        <MyHeader />
+        <Bar />
+      </Outter>
+    </React.Fragment>
+  );
 };
-const Outter=styled.div`
-  width:100%;
-  padding-bottom:68px;
-
+const Outter = styled.div`
+  width: 100%;
+  padding-bottom: 68px;
 `;
 
 export default Like;
