3e48d9c58a4d6fcb91e39a980c1ead7a4b7bf008
ryurim0109 <dbfla5036@naver.com>
2022-05-02 16:29:06 +0900

    [style] mypage view 완성_yr[220502]

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Bar} from '../components/home';
+import {MyHeader,MyProfile,MyContent,MyLogout} from '../components/my/index'
 
 const MyPage = () => {
     return (
         <React.Fragment>
             <Outter>
+                <MyHeader/>
+                <MyProfile/>
+                <MyContent/>
+                <MyLogout/>
                 <Bar/>
             </Outter>
         </React.Fragment>
