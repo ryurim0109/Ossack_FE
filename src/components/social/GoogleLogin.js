f311d4ccea4771557a93daff8549dd3beba5870e
devkevinsoon <parkts7777@gmail.com>
2022-05-04 19:44:41 +0900

    [feat] social Login 추가_pts20220504

---
+++ src/components/social/GoogleLogin.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../../redux/modules/user";
+
+const GoogleLogin = (props) => {
+  const dispatch = useDispatch();
+
+  // 인가코드
+  let code = new URL(window.location.href).searchParams.get("code");
+  const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_ID;
+  console.log("code : ", GOOGLE_CLIENT_ID);
+  React.useEffect(() => {
+    dispatch(userActions.loginBygoogle(code));
+  }, []);
+
+  return null;
+};
+
+export default GoogleLogin;
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,8 +3,18 @@ import styled from "styled-components";
 import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configStore";
-import { Login, Main, Signup, SaleMap,MyPage,Like,Start } from "../pages/index";
+import {
+  Login,
+  Main,
+  Signup,
+  SaleMap,
+  MyPage,
+  Like,
+  Start,
+} from "../pages/index";
 import { MobileFrame } from "../components/shared/home";
+
+import GoogleLogin from "../components/social/GoogleLogin";
 
 //css
 import "../shared/css/App.css";
@@ -18,7 +28,7 @@ function App() {
             <Route path="/" exact component={Start} />
             <Route path="/login" exact component={Login} />
             <Route path="/signup" exact component={Signup} />
-
+            <Route path="/user/google/callback" component={GoogleLogin} />
             <Route path="/main" exact component={Main} />
             <Route path="/map" exact component={SaleMap} />
             <Route path="/mypage" exact component={MyPage} />
