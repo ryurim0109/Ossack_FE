80a5a3177d9d8b7a7270cc550b7076635f2caa64
devkevinsoon <parkts7777@gmail.com>
2022-05-04 14:02:05 +0900

    [test] bottom sheet test_pts20220504

--- src/components/motion/BottomSheet.js
+++ src/components/motion/BottomSheet.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 /**
  * Animations
  */
@@ -13,39 +13,43 @@ import { grey600 } from "material-ui/styles/colors";
  * Local styles
  */
 import styles from "./style";
+import { useEffect } from "react";
 
-class BottomSheet extends Component {
-  state = {
+const BottomSheet = (props) => {
+  console.log("props : ", props);
+
+  const [state, setState] = useState({
+    //기본 설정값
     opacity: 0,
     translate: 100,
     display: "hidden",
-  };
+  });
 
-  componentWillMount() {
-    if (this.props.startHidden === false) {
-      this.setState({
+  useEffect(() => {
+    if (props.startHidden === false) {
+      setState({
         opacity: 0.5,
         translate: 0,
         display: "visible",
       });
     }
-  }
+  }, []);
 
-  animate = () => {
-    this.setState(
+  const animate = () => {
+    setState(
       {
-        opacity: this.state.opacity === 0.5 ? 0 : 0.5,
-        translate: this.state.opacity === 0 ? 0 : 100,
+        opacity: state.opacity === 0.5 ? 0 : 0.5,
+        translate: state.opacity === 0 ? 0 : 100,
       },
       () => {
-        if (this.state.opacity === 0) {
+        if (state.opacity === 0) {
           setTimeout(() => {
-            this.setState({
+            setState({
               display: "hidden",
             });
           }, 200);
         } else {
-          this.setState({
+          setState({
             display: "visible",
           });
         }
@@ -53,83 +57,95 @@ class BottomSheet extends Component {
     );
   };
 
-  render() {
-    return (
-      <div>
-        {React.cloneElement(this.props.buttonElement, {
-          onClick: this.animate,
-        })}
-        <Motion
-          style={{
-            opacity: spring(this.state.opacity),
-            translate: spring(this.state.translate),
-          }}
-        >
-          {({ opacity, translate }) => (
+  return (
+    <div>
+      {React.cloneElement(props.buttonElement, {
+        onClick: animate,
+      })}
+      <Motion
+        style={{
+          opacity: spring(state.opacity),
+          translate: spring(state.translate),
+        }}
+      >
+        {({ opacity, translate }) => (
+          <div
+            style={Object.assign({}, styles.container, {
+              visibility: state.display,
+            })}
+            onClick={animate}
+          >
             <div
-              style={Object.assign({}, styles.container, {
-                visibility: this.state.display,
+              style={Object.assign({}, styles.backgroundContainer, {
+                opacity: opacity,
               })}
-              onClick={this.animate}
+            />
+            <ul
+              style={Object.assign({}, styles.list, {
+                transform: `translateY(${translate}%)`,
+              })}
             >
               <div
-                style={Object.assign({}, styles.backgroundContainer, {
-                  opacity: opacity,
-                })}
-              />
-              <ul
-                style={Object.assign({}, styles.list, {
-                  transform: `translateY(${translate}%)`,
-                })}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  paddingTop: "0.5rem",
+                  paddingBottom: "1rem",
+                }}
               >
-                <div
+                <span
                   style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    paddingTop: "0.5rem",
-                    paddingBottom: "1rem",
+                    display: "block",
+                    height: "3px",
+                    width: "28px",
+                    backgroundColor: "rgba(0, 0, 0, 0.3)",
+                    borderRadius: "10px",
+                    margin: "auto",
                   }}
-                >
-                  <span
-                    style={{
-                      display: "block",
-                      height: "3px",
-                      width: "28px",
-                      backgroundColor: "rgba(0, 0, 0, 0.3)",
-                      borderRadius: "10px",
-                      margin: "auto",
-                    }}
-                  ></span>
-                </div>
-                {this.props.items.map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <button
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          item.onClick.call(this, this.animate.bind(this));
-                        }}
-                        style={styles.button}
-                      >
-                        {React.cloneElement(item.icon, {
-                          style: {
-                            marginRight: "36px",
-                            color: grey600,
-                          },
-                        })}
-                        <span style={styles.text}>{item.text}</span>
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          )}
-        </Motion>
-      </div>
-    );
-  }
-}
+                ></span>
+              </div>
+              {this.props.items.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        item.onClick.call(this, animate.bind(this));
+                      }}
+                      style={styles.button}
+                    >
+                      {React.cloneElement(item.icon, {
+                        style: {
+                          marginRight: "36px",
+                          color: grey600,
+                        },
+                      })}
+                      <span style={styles.text}>{item.text}</span>
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </Motion>
+    </div>
+  );
+};
+
+// class BottomSheet extends Component {
+//   state = {
+
+//   };
+
+//   componentWillMount() {
+
+//   }
+
+//   render() {
+
+//   }
+// }
 
 export default BottomSheet;
