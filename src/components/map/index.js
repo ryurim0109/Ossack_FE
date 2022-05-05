ae7c0f4f0f081e3342e2547c7491b6c34a87bbd8
ryurim0109 <dbfla5036@naver.com>
2022-05-05 21:33:17 +0900

    [style]필터 파일 생성_yr[220505]

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -2,5 +2,6 @@ import MainMap from "./MainMap";
 import Position from "./Position";
 import Search from "./Search";
 import Overlay from "./Overlay";
+import Filter from "./Filter";
 
-export { MainMap, Position, Search, Overlay };
+export { MainMap, Position, Search, Overlay, Filter };
