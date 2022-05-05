267f9bd26cc45e33db4bd86b023655e3999addd6
ryurim0109 <dbfla5036@naver.com>
2022-05-05 13:04:41 +0900

    [style]간격 수정_yr[220505]

--- src/components/main/SaleList.js
+++ src/components/main/SaleList.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Grid} from '../../elements/index';
+import {Grid,Text} from '../../elements/index';
 
 const SaleList = () => {
     return (
         <React.Fragment>
-            <Grid margin="24px 0"> 
+            <Grid margin="0 0 32px 0"> 
                 <Grid display="flex" justifyContent="space-between" margin="16px 0">
-                    <Grid width="47.4%" height="190px" border="1px solid #E5E5EC" borderRadius="8px" 
-                    boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)"></Grid>
-                    <Grid width="47.4%" height="190px"  border="1px solid #E5E5EC" borderRadius="8px" 
-                    boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)"></Grid>
+                    <Grid width="47.4%" height="160px" border="1px solid #E5E5EC" borderRadius="8px" 
+                    boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)" padding="12px">
+                        <Text size="1.250rem" bold>오피스</Text>
+                    </Grid>
+                    <Grid width="47.4%" height="160px"  border="1px solid #E5E5EC" borderRadius="8px" 
+                    boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)" padding="12px">
+                         <Text size="1.250rem" bold>오피스텔</Text>
+                    </Grid>
                 </Grid>
             </Grid>
         </React.Fragment>
