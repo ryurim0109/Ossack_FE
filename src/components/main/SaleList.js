55730e619812816e63f7e4633dd19f0cef0a2000
ryurim0109 <dbfla5036@naver.com>
2022-05-06 13:09:02 +0900

    [style] history추가 _yr[220506]

--- src/components/main/SaleList.js
+++ src/components/main/SaleList.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {Grid,Text} from '../../elements/index';
+import { history } from '../../redux/configStore';
 
 const SaleList = () => {
     return (
         <React.Fragment>
             <Grid margin="0 0 32px 0"> 
-                <Grid display="flex" justifyContent="space-between" margin="16px 0">
+                <Grid display="flex" justifyContent="space-between" margin="16px 0" cursor="pointer" _onClick={()=>{
+                    history.push('/map')
+                }}>
                     <Grid width="47.4%" height="160px" border="1px solid #E5E5EC" borderRadius="8px" 
                     boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)" padding="12px">
                         <Text size="1.250rem" bold>오피스</Text>
                     </Grid>
-                    <Grid width="47.4%" height="160px"  border="1px solid #E5E5EC" borderRadius="8px" 
+                    <Grid width="47.4%" height="160px"  border="1px solid #E5E5EC" borderRadius="8px" cursor="pointer" _onClick={()=>{
+                    history.push('/map')
+                }}
                     boxShadow="0px 2px 4px rgba(105, 105, 105, 0.1)" padding="12px">
                          <Text size="1.250rem" bold>오피스텔</Text>
                     </Grid>
