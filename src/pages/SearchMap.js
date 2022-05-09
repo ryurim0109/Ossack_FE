c48102c24b2a6e6b1da7cc5545f78c49761b555e
devkevinsoon <parkts7777@gmail.com>
2022-05-09 21:35:26 +0900

    [style] SearchMap 수정_pts20220509

--- src/pages/SearchMap.js
+++ src/pages/SearchMap.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { MyHeader } from '../components/my/index';
-import { OneMap } from '../components/map/index';
+import React from "react";
+import { MyHeader } from "../components/my/index";
+import { OneMap } from "../components/map/index";
 
 const SearchMap = () => {
-    return (
-        <React.Fragment>
-            <MyHeader>리스트</MyHeader>
-            <OneMap/>
-            
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <MyHeader>리스트</MyHeader>
+      <OneMap />
+    </React.Fragment>
+  );
 };
 
 export default SearchMap;
