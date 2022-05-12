e4b779308b56695256f5bc8308c3e6d32571acd8
ryurim0109 <dbfla5036@naver.com>
2022-05-13 00:01:53 +0900

    [style]스플래시 적용_yr[220513]

---
+++ src/pages/Splash.js
@@ -0,0 +1,51 @@
+import React,{useEffect} from "react";
+import { Grid, Image } from "../elements/index";
+import styled from "styled-components";
+import { history } from "../redux/configStore";
+import moomgu from '../static/images/logo01.svg';
+import ossack from '../static/images/logo02.svg';
+
+const Splash = () => {
+  // 2초 뒤 채팅 메인으로 넘어가는 함수 작성
+  const timeout = () => {
+    setTimeout(() => {
+      history.push('/start');
+    }, 2000);
+  };
+  // 컴포넌트가 화면에 다 나타나면 timeout 함수 실행
+  useEffect(() => {
+    timeout();
+    return () => {
+      // clear 해줌
+      clearTimeout(timeout);
+    };
+  });
+  return (
+    <React.Fragment>
+      
+        <Grid
+          width="100%"
+          padding="38px 0"
+          display="flex"
+          flexDirection="column"
+          bg="#3E00FF"
+        >
+          <Grid width="100%" margin="118px 0" height="237px" position="relative">
+            <img  src={ossack} alt="오싹 이미지"/>
+            <Grid width="217px" height="14px" position="absolute" top="0" right="15px">
+              <Image shape="rectangle" padding="14px" src={moomgu}/>
+            </Grid>
+          </Grid>
+        </Grid>
+      
+    </React.Fragment>
+  );
+};
+const Outter = styled.div`
+  width: 100%;
+  padding: 0 16px 68px;
+`;
+const A = styled.a`
+  color: #000;
+`;
+export default Splash;
