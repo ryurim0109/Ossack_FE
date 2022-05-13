532ede04b93bf768562d6d6d1935a03cc827094a
ryurim0109 <dbfla5036@naver.com>
2022-05-13 11:43:19 +0900

    [style] 보더 스타일 없앰_yr[220513]

--- src/components/my/MyLogout.js
+++ src/components/my/MyLogout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Button,Grid,Text} from '../../elements/index';
+import {Button,Grid} from '../../elements/index';
 
 const MyLogout = () => {
     return (
         <React.Fragment>
             <Grid width="100%" display="flex" margin="120px 0 0 0" padding="0 107px" alignItems="center" justifyContent="space-between">
-                <Button width="49px" fontSize="0.875rem" color="#999" backgroundColor="none">로그아웃</Button>
-                <Button  width="49px"fontSize="0.875rem" color="#999" backgroundColor="none">회원탈퇴</Button>
+                <Button border="none" width="49px" fontSize="0.875rem" color="#999" backgroundColor="none">로그아웃</Button>
+                <Button border="none"  width="49px"fontSize="0.875rem" color="#999" backgroundColor="none">회원탈퇴</Button>
             </Grid>
         </React.Fragment>
     );
