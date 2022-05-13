6269d41da692167f36aabcda3cfb0150c4ff767e
ryurim0109 <dbfla5036@naver.com>
2022-05-14 02:18:58 +0900

    [style]말풍선 디자인 컴포넌트로 하나 빼둠_yr[220514]

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Grid, Button, Text, Image } from "../elements/index";
+import { Grid, Button, Text } from "../elements/index";
 import styled from "styled-components";
 import { history } from "../redux/configStore";
 import { KAKAO_AUTH_URL, GOOGLE_AUTH_URL } from "../shared/SocialOAuth";
+import { TalkTalk } from "../components/main/index";
 
 import ossack from "../static/images/logo03.svg";
 import kakaoIcon from "../static/images/kakaoIcon.svg";
@@ -88,9 +89,7 @@ const Start = () => {
                 margin="16px 0 0"
                 position="relative"
               >
-                <Talk>
-                  <Text color="#FF6868">3초면</Text> 빠른 로그인 완료!
-                </Talk>
+                <TalkTalk/>
                 <Grid
                   width="114px"
                   display="flex"
@@ -129,47 +128,5 @@ const P = styled.p`
   cursor: pointer;
   font-size: 0.75rem;
 `;
-const Talk = styled.div`
-  color: #fff;
-  width: 134px;
-  height: 28px;
-  background-color: #3e00ff;
-  top: -36px;
-  border: 3px solid #3e00ff;
-  font-size: 0.75rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: 50%;
-  border-radius: 10px;
-  transform: translateX(-50%);
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
-  animation-name: bounceX;
-  z-index: 99;
 
-  &:before {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    width: 0;
-    height: 0;
-    border: 9px solid transparent;
-    border-top-color: #3e00ff;
-    border-bottom: 0;
-    margin-left: -9px;
-    margin-bottom: -9px;
-  }
-
-  @keyframes bounceX {
-    50% {
-      top: -30px;
-    }
-    100% {
-      top: -36px;
-    }
-  }
-`;
 export default Start;
