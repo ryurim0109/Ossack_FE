05b409e9ea9330a956884b42bc4de66689b22e68
ryurim0109 <dbfla5036@naver.com>
2022-05-14 11:44:08 +0900

    [feat]redirect_uri수정_yr220514

--- src/shared/SocialOAuth.js
+++ src/shared/SocialOAuth.js
@@ -1,9 +1,11 @@
 // kakao Login
 const CLIENT_ID = process.env.REACT_APP_KAKAO_ID;
+// const REDIRECT_URI = "https://d389mk5b2t00ww.cloudfront.net/user/kakao/callback";
 const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
 
 // google Login
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_ID;
+// const GOOGLE_REDIRECT_URI = "https://d389mk5b2t00ww.cloudfront.net/user/google/callback";
 const GOOGLE_REDIRECT_URI = "http://localhost:3000/user/google/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
