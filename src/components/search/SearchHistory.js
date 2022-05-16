16fa92e4e5d42259f141283e441c0a0e4814a834
devkevinsoon <parkts7777@gmail.com>
2022-05-17 00:57:24 +0900

    [style] style component 정리_pts20220517

--- src/components/search/SearchHistory.js
+++ src/components/search/SearchHistory.js
@@ -1,6 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
+function History({ keywords, onRemoveKeyword, onClearKeywords }) {
+  console.log("keyword", keywords);
+  if (keywords.length === 0) {
+    return <HistoryContainer>최근 검색된 기록이 없습니다.</HistoryContainer>;
+  }
+  return (
+    <HistoryContainer>
+      <HeaderContainer>
+        <Title>최근 검색어</Title>
+        <RemoveText onClick={onClearKeywords}>전체삭제</RemoveText>
+      </HeaderContainer>
+      <ListContainer>
+        {keywords.map(({ id, text }) => {
+          return (
+            <KeywordContainer key={id}>
+              <Keyword>{text}</Keyword>
+              <RemoveButton
+                //눌렸을때 해야하는거라 arrow function을 사용하여 실행
+                //그냥 함수 쓰면은 그려지자마자 바로 실행됨
+                onClick={() => {
+                  onRemoveKeyword(id);
+                }}
+              >
+                삭제
+              </RemoveButton>
+            </KeywordContainer>
+          );
+        })}
+      </ListContainer>
+    </HistoryContainer>
+  );
+}
+
 const HistoryContainer = styled.div`
   padding: 18px;
 `;
@@ -44,38 +77,5 @@ const Keyword = styled.span`
   font-size: 18px;
   font-weight: 400;
 `;
-
-function History({ keywords, onRemoveKeyword, onClearKeywords }) {
-  console.log("keyword", keywords);
-  if (keywords.length === 0) {
-    return <HistoryContainer>최근 검색된 기록이 없습니다.</HistoryContainer>;
-  }
-  return (
-    <HistoryContainer>
-      <HeaderContainer>
-        <Title>최근 검색어</Title>
-        <RemoveText onClick={onClearKeywords}>전체삭제</RemoveText>
-      </HeaderContainer>
-      <ListContainer>
-        {keywords.map(({ id, text }) => {
-          return (
-            <KeywordContainer key={id}>
-              <Keyword>{text}</Keyword>
-              <RemoveButton
-                //눌렸을때 해야하는거라 arrow function을 사용하여 실행
-                //그냥 함수 쓰면은 그려지자마자 바로 실행됨
-                onClick={() => {
-                  onRemoveKeyword(id);
-                }}
-              >
-                삭제
-              </RemoveButton>
-            </KeywordContainer>
-          );
-        })}
-      </ListContainer>
-    </HistoryContainer>
-  );
-}
 
 export default History;
