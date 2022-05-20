4d8fddba636671ef034f1efb1bfb03b002058f69
ryurim0109 <dbfla5036@naver.com>
2022-05-20 16:53:17 +0900

    [style]로그아웃 팝업창 연결_yr220520

--- src/components/my/index.js
+++ src/components/my/index.js
@@ -1,8 +1,8 @@
-import MyHeader from './MyHeader';
-import MyProfile from './MyProfile';
-import MyContent from './MyContent';
-import MyLogout from './MyLogout';
-import ProfileModal from './ProfileModal';
+import MyHeader from "./MyHeader";
+import MyProfile from "./MyProfile";
+import MyContent from "./MyContent";
+import MyLogout from "./MyLogout";
+import ProfileModal from "./ProfileModal";
+import LogoutModal from "./LogoutModal";
 
-
-export { MyHeader,MyProfile,MyContent,MyLogout,ProfileModal };
+export { MyHeader, MyProfile, MyContent, MyLogout, ProfileModal, LogoutModal };
