536987e0e0fb7038c367afb0dd1050754500e00f
ryurim0109 <dbfla5036@naver.com>
2022-05-23 16:52:42 +0900

    [style]멤버 이미지 추가-yr220523

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { MyHeader } from "../components/my/index";
-import mem from "../assets/member1.png";
+//멤버 이미지
+import member1 from "../assets/member1.png";
+import member2 from "../assets/member2.png";
+import member3 from "../assets/member3.png";
+import member4 from "../assets/member4.png";
+import member5 from "../assets/member5.png";
+import member6 from "../assets/member6.png";
+import member7 from "../assets/member7.png";
+import member8 from "../assets/member8.png";
+import member9 from "../assets/member9.png";
 import { Grid } from "../elements/index";
 
 const Member = () => {
@@ -8,7 +17,7 @@ const Member = () => {
     <React.Fragment>
       <MyHeader>오싹 팀원소개</MyHeader>
       <Grid width="120px" height="120px">
-        <img src={mem} alt="민우" />
+        <img src={member1} alt="민우" />
       </Grid>
     </React.Fragment>
   );
