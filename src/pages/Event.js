f19e47268ef95eb76145312684c9ae0b7e643310
ryurim0109 <dbfla5036@naver.com>
2022-05-24 03:07:06 +0900

    [style]이벤트 페이지 완성_yr220524

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { Grid } from "../elements/index";
 import styled from "styled-components";
 import eventImg from "../assets/eventImg.png";
+import eventBg from "../assets/eventbg.png";
 
 const Event = () => {
   return (
     <React.Fragment>
-      <Grid width="100%" height="510px">
-        <img src={eventImg} alt="이벤트 사진" />
-      </Grid>
+      <Outter>
+        <Inner
+          onClick={() => {
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSdZy-5M5ckomAUZt-aTh6mtfmHMtqoHmU8I1L3frJ9GWjpjiw/viewform",
+              "_blank"
+            );
+          }}
+        >
+          <img src={eventImg} alt="이벤트 사진" />
+        </Inner>
+      </Outter>
     </React.Fragment>
   );
 };
-
+const Outter = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${eventBg});
+`;
+const Inner = styled.div`
+  width: 100%;
+  height: 510px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`;
 export default Event;
