9793e9e0f299fd60a055ae0e33c290a4112f47bb
ryurim0109 <dbfla5036@naver.com>
2022-05-23 14:06:29 +0900

    [style]이미지 뺌_yr220523

--- src/components/main/HotPlaceList.js
+++ src/components/main/HotPlaceList.js
@@ -7,14 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { history } from "../../redux/configStore";
 import img01 from "../../assets/1.jpg";
 import img02 from "../../assets/2.jpg";
-import img03 from "../../assets/3.jpg";
-import img04 from "../../assets/4.jpg";
-import img05 from "../../assets/5.jpg";
-
 const PlaceList = () => {
   const dispatch = useDispatch();
-  const HotList = useSelector((state) => state.office.hot_list);
-  console.log(HotList);
   useEffect(() => {
     dispatch(officeActions.getHotDB());
   }, []);
@@ -24,22 +18,6 @@ const PlaceList = () => {
       <Grid overflow="hidden" width="100%">
         <XScrollDrag>
           <FlexBox>
-            {/* {HotList && HotList?.map((o,idx)=>{
-                        return(
-                            <Grid key={idx} width="160px" height="180px"  borderRadius="8px" position="relative" overflow="hidden">
-                                <Image padding="180px" src={idx} shape="rectangle" position="absolute"/>
-                                <Grid  position="absolute" bottom="0" width="100%" height="68px" bg="rgba(0, 0, 0, 0.35)" padding="8px 16px 16px">
-                                    <Grid width="42px" height="20px" bg="#fff" borderRadius="42px" display="flex" alignItems="center" justifyContent="center">
-                                        <Text color="#111" size="10px">{o.monthly?o.monthly:"dd"}</Text>
-                                    </Grid>
-                                    <Grid color="#fff" fontSize="10px" padding="4px" >
-                                        <Text bold>{o.building_info?o.building_info:"맛집거리가 눈앞에 펼쳐지는 맛집거리 오피스"}</Text>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        )
-                    })} */}
-            {/* map돌릴 부분 */}
             <Grid
               width="160px"
               height="180px"
@@ -60,9 +38,9 @@ const PlaceList = () => {
                 height="68px"
                 bg="rgba(0, 0, 0, 0.35)"
                 padding="8px 16px 16px"
-                _onClick={() => {
-                  history.push(`/detail/${HotList[0]?.estateid}`);
-                }}
+                // _onClick={() => {
+                //   history.push(`/detail/${HotList[0]?.estateid}`);
+                // }}
               >
                 <Grid
                   width="42px"
@@ -73,179 +51,11 @@ const PlaceList = () => {
                   alignItems="center"
                   justifyContent="center"
                 >
-                  <Text size="10px">{HotList[0]?.monthly}</Text>
+                  <Text size="10px">을지로</Text>
                 </Grid>
                 <Grid fontSize="10px" padding="4px">
                   <Text color="#fff" bold>
-                    {HotList[0]?.building_info}
-                  </Text>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid
-              width="160px"
-              height="180px"
-              borderRadius="8px"
-              position="relative"
-              overflow="hidden"
-            >
-              <Image
-                padding="180px"
-                src={img02}
-                shape="rectangle"
-                position="absolute"
-              />
-              <Grid
-                position="absolute"
-                bottom="0"
-                width="100%"
-                height="68px"
-                bg="rgba(0, 0, 0, 0.35)"
-                padding="8px 16px 16px"
-                _onClick={() => {
-                  history.push(`/detail/${HotList[1]?.estateid}`);
-                }}
-              >
-                <Grid
-                  width="42px"
-                  height="20px"
-                  bg="#fff"
-                  borderRadius="42px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Text size="10px">{HotList[1]?.monthly}</Text>
-                </Grid>
-                <Grid fontSize="10px" padding="4px">
-                  <Text color="#fff" bold>
-                    {HotList[1]?.building_info}
-                  </Text>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid
-              width="160px"
-              height="180px"
-              borderRadius="8px"
-              position="relative"
-              overflow="hidden"
-            >
-              <Image
-                padding="180px"
-                src={img03}
-                shape="rectangle"
-                position="absolute"
-              />
-              <Grid
-                position="absolute"
-                bottom="0"
-                width="100%"
-                height="68px"
-                bg="rgba(0, 0, 0, 0.35)"
-                padding="8px 16px 16px"
-                _onClick={() => {
-                  history.push(`/detail/${HotList[2]?.estateid}`);
-                }}
-              >
-                <Grid
-                  width="42px"
-                  height="20px"
-                  bg="#fff"
-                  borderRadius="42px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Text size="10px">{HotList[2]?.monthly}</Text>
-                </Grid>
-                <Grid fontSize="10px" padding="4px">
-                  <Text color="#fff" bold>
-                    {HotList[2]?.building_info}
-                  </Text>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid
-              width="160px"
-              height="180px"
-              borderRadius="8px"
-              position="relative"
-              overflow="hidden"
-            >
-              <Image
-                padding="180px"
-                src={img04}
-                shape="rectangle"
-                position="absolute"
-              />
-              <Grid
-                position="absolute"
-                bottom="0"
-                width="100%"
-                height="68px"
-                bg="rgba(0, 0, 0, 0.35)"
-                padding="8px 16px 16px"
-                _onClick={() => {
-                  history.push(`/detail/${HotList[3]?.estateid}`);
-                }}
-              >
-                <Grid
-                  width="42px"
-                  height="20px"
-                  bg="#fff"
-                  borderRadius="42px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Text size="10px">{HotList[3]?.monthly}</Text>
-                </Grid>
-                <Grid fontSize="10px" padding="4px">
-                  <Text color="#fff" bold>
-                    {HotList[3]?.building_info}
-                  </Text>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid
-              width="160px"
-              height="180px"
-              borderRadius="8px"
-              position="relative"
-              overflow="hidden"
-            >
-              <Image
-                padding="180px"
-                src={img05}
-                shape="rectangle"
-                position="absolute"
-              />
-              <Grid
-                position="absolute"
-                bottom="0"
-                width="100%"
-                height="68px"
-                bg="rgba(0, 0, 0, 0.35)"
-                padding="8px 16px 16px"
-                _onClick={() => {
-                  history.push(`/detail/${HotList[4]?.estateid}`);
-                }}
-              >
-                <Grid
-                  width="42px"
-                  height="20px"
-                  bg="#fff"
-                  borderRadius="42px"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Text size="10px">{HotList[4]?.monthly}</Text>
-                </Grid>
-                <Grid fontSize="10px" padding="4px">
-                  <Text color="#fff" bold>
-                    {HotList[4]?.building_info}
+                    힙한 직장인들의 성지 힙지로라 불리는 을지로 오피스
                   </Text>
                 </Grid>
               </Grid>
