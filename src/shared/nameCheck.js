db56cce1b197a9d02f49f87310b9289cc11473e4
devkevinsoon <parkts7777@gmail.com>
2022-05-25 21:20:27 +0900

    [feat] 정규식 표현 추가_pts20220525

--- src/shared/nameCheck.js
+++ src/shared/nameCheck.js
@@ -3,3 +3,9 @@ export const checkName = (nickname) => {
   var regName = /^[가-힣a-zA-Z]{2,10}$/;
   return regName.test(nickname);
 };
+
+// 검색어 체크 (특수문자 제외)
+export const searchCheck = (search) => {
+  var regSearch = /^[가-힣]{1,20}$/;
+  return regSearch.test(search);
+};
