85e15501d919e45874aa2ce54a6076ac5e579aac
devkevinsoon <parkts7777@gmail.com>
2022-05-25 17:12:15 +0900

    [feat] ScrollToTop 구현중_pts20220525

--- src/components/shared/MobileFrame.js
+++ src/components/shared/MobileFrame.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../../shared/css/MobileFrame.css";
 
-const MobileFrame = ({ children }) => { 
-  return (  
-    <React.Fragment>  
-    <div className="WebFullFrame" >
-      <div className="MobileFullFrame" >
-        <div id="scroll" className="Container" >{children}</div>
+import { useLocation } from "react-router-dom";
+
+const MobileFrame = ({ children }) => {
+  const { pathname } = useLocation();
+  const mobileFrame = useRef();
+
+  console.log("path : ", pathname);
+
+  useEffect(() => {
+    // const element = document.getElementById("mobileFrame");
+    // element.scrollTo(0, 0);
+
+    mobileFrame.current.scrollTo(0, 0);
+    console.log("called");
+
+    // window.scrollTo(0, 0);
+    // console.log("called");
+  }, [pathname]);
+
+  return (
+    <React.Fragment>
+      <div className="WebFullFrame">
+        <div className="MobileFullFrame" ref={mobileFrame}>
+          <div id="scroll" className="Container">
+            {children}
+          </div>
+        </div>
       </div>
-    </div>
     </React.Fragment>
   );
 };
