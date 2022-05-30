534a16e14bfea242944bec97cbacc07ce29b1203
ryurim0109 <dbfla5036@naver.com>
2022-05-30 20:02:44 +0900

    [refactor]주석 정리_yr220530

--- src/components/shared/SlickSlider.js
+++ src/components/shared/SlickSlider.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import Slider from "react-slick";
-/* import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"; */
 import { ReactComponent as Next } from "../../assets/next.svg";
 import { ReactComponent as Prev } from "../../assets/prev.svg";
 import MagicSliderDots from "react-magic-slider-dots";
