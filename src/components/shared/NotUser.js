f84d45d0ef635d9117eb2dc7f648ba4c9a8fec0f
ryurim0109 <dbfla5036@naver.com>
2022-05-31 17:17:53 +0900

    [style]로그인 안한 유저에게만 보이는 페이지 완성_yr220531

--- src/components/shared/NotUser.js
+++ src/components/shared/NotUser.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Grid, Image, Text, Button } from "../../elements/index";
-import ossack from "../../assets/ossack02.jpg";
+import ossack from "../../assets/login.png";
 
 const NotUser = () => {
   return (
@@ -12,30 +12,39 @@ const NotUser = () => {
           flexDirection="column"
           justifyContent="space-between"
           alignItems="center"
-          height="400px"
-          padding="50px 16px"
+          height="370px"
+          padding="0 16px"
         >
           <Grid
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            height="117px"
-          >
-            <Image src={ossack} size="117" />
-          </Grid>
-          <Grid
-            height="50px"
+            height="155px"
             display="flex"
             flexDirection="column"
-            justifyContent="center"
+            justifyContent="space-between"
             alignItems="center"
           >
-            <Text size="14px" color="#808080">
-              로그인한 회원만 볼 수 있어요,
-            </Text>
-            <Text size="14px" color="#808080">
-              로그인 페이지로 이동해주시겠어요?
-            </Text>
+            <Grid
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              width="109px"
+              height="98px"
+            >
+              <img src={ossack} alt="경고 이미지" />
+            </Grid>
+            <Grid
+              height="40px"
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text size="14px" bold>
+                로그인한 회원만 볼 수 있어요.
+              </Text>
+              <Text size="14px" bold>
+                로그인 페이지로 이동해주시겠어요?
+              </Text>
+            </Grid>
           </Grid>
           <Button
             borderRadius="8px"
